b6b3664356a0d92115fdc7828e0a3a7921aed7f2
Tyuusaku777 <mikagamit777@gmail.com>
2019-08-10 14:21:35 +0900

    migartion fixed

--- db/migrate/20190801122921_add_residence_to_users.rb
+++ db/migrate/20190801122921_add_residence_to_users.rb
@@ -1,5 +1,5 @@
 class AddResidenceToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :residence, :integer, :limit=>46
+    add_column :users, :residence, :integer
   end
 end
