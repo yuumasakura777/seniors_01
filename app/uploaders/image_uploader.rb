2538771c4ef0ee1053f2e3ed2480714d33662842
Tyuusaku777 <mikagamit777@gmail.com>
2019-08-13 07:27:11 +0900

    alter carriewave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,7 +11,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   #amazon S3に保存
   storage :fog
-  # storage :fog
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,22 @@
 
-
-#require 'carrierwave/storage/abstract'
- require 'carrierwave/storage/file'
- require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage= :fog
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AKIAX7C2D3UF5SDU7G7M'],
-      aws_secret_access_key: ENV['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'],
-      region: ENV['ap-northeast-1'],
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAX7C2D3UF5SDU7G7M'],
+      :aws_secret_access_key => ENV['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'],
+      :region                => ENV['ap-northeast-1'],
     }
-    config.fog_directory= ENV['mikagamit777']
-    config.fog_public= false
-    config.fog_attributes= {cache_control: "public, max-age=#{365.days.to_i}" }
+
+    config.fog_directory = ENV['mikagamit777']
+    config.fog_public = true
   end
 else
   CarrierWave.configure do |config|
