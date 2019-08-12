bb4aae9a1b898bdf35a94c08858361f37b36d984
Tyuusaku777 <mikagamit777@gmail.com>
2019-08-12 19:13:25 +0900

    alter assets.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAX7C2D3UF5SDU7G7M',
-    aws_secret_access_key: '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',
+    aws_access_key_id: ENV['AKIAX7C2D3UF5SDU7G7M'],
+    aws_secret_access_key: ENV['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'],
     region: 'ap-northeast-1',
     path_style: true
   }
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
   config.cache_storage = :fog
 
   config.fog_directory = 'mikagamit777'
-  config.asset_host = 'mikagamit777'
+  #config.asset_host = 'mikagamit777'
 
 end
 # 日本語の文字化けを防ぐ
