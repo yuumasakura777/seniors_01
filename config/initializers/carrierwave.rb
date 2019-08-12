bbbbd29fc176e89ab4d26091533bee4af931ce65
Tyuusaku777 <mikagamit777@gmail.com>
2019-08-12 22:46:23 +0900

    alter carriewave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 
 
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+#require 'carrierwave/storage/abstract'
+#require 'carrierwave/storage/file'
+#require 'carrierwave/storage/fog'
 
 
 if Rails.env.production?
