dd7f4113bb8b0f9c8a183aa246547c6d75acf58e
Tyuusaku777 <mikagamit777@gmail.com>
2019-08-13 19:09:44 +0900

    alter carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,11 +10,11 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
-      :aws_secret_access_key =>'R/v4Yz4jg0UBfk9+CykLlJRoGEIoD9Yql7b31yBF',
-      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => 'AKIAX7C2D3UF5SDU7G7M',
+      :aws_secret_access_key => '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',
+      :region                => 'ap-northeast-1',
     }
-    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.fog_directory = 'mikagamit777'
     config.fog_public = true
   end
 end
